95f7cdc7e68320e097ce56e922dd4ad2d5ec1b36
rukieya33 <155789904+rukieya33@users.noreply.github.com>
2024-05-16 17:36:57 +0100

    grbot-backend

---
+++ Migrations/20240516100213_MyFirstMigration.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace REST_BOT.Migrations
+{
+    public partial class MyFirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "planners",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    activity = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    day = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    startTime = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    endTime = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    breakTime = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_planners", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "planners");
+        }
+    }
+}
