9c4cd95e477910131ebb89725ba18977d62a731b
rukieya33 <155789904+rukieya33@users.noreply.github.com>
2024-05-16 17:40:11 +0100

    grbot-backend

---
+++ Program.cs
@@ -0,0 +1,40 @@
+using Microsoft.EntityFrameworkCore;
+using REST_BOT.Models;
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:3000"
+                                              )
+                                .AllowAnyHeader()
+                                .AllowAnyMethod();
+                      });
+});
+// Add services to the container.
+
+builder.Services.AddControllers();
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<DayPlannerContext>(options => options.UseSqlServer(connectionString));
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseCors(MyAllowSpecificOrigins);
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
