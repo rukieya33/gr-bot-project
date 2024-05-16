8a061e5becd2124e60e6ae3cc016dc9208484ea7
rukieya33 <155789904+rukieya33@users.noreply.github.com>
2024-05-16 17:37:32 +0100

    grbot-backend

---
+++ Models/DayPlanner.cs
@@ -0,0 +1,18 @@
+﻿namespace REST_BOT.Models
+{
+    public class DayPlanner
+    {
+
+        public int Id { get; set; }
+        public string? activity { get; set; }
+
+        public string? day { get; set; }
+
+        public string? startTime { get; set; }
+
+        public string? endTime { get; set; }
+
+        public string? breakTime { get; set; }
+       
+    }
+}
---
+++ Models/DayPlannerContext.cs
@@ -0,0 +1,17 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace REST_BOT.Models
+{
+    public class DayPlannerContext : DbContext
+    {
+   
+        public DayPlannerContext(DbContextOptions<DayPlannerContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<DayPlanner> planners { get; set; } = null!;
+    }
+}
